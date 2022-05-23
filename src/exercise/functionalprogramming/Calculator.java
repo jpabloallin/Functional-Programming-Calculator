ddc35b73b9ed891b9f37773ce2aab9319bf003d5
jpabloallin <jpac.647@gmail.com>
2022-05-23 18:42:40 -0500

    - Created objects from IAddition and ISubtraction interfaces. - Overrode methods with a lambda function in order to specify the formula of each calculation. - Sent parameter values by calling the method withing the interfaces objects.

--- src/exercise/functionalprogramming/Calculator.java
+++ src/exercise/functionalprogramming/Calculator.java
@@ -2,6 +2,21 @@ package exercise.functionalprogramming;
 
 public class Calculator {
     public static void main(String[] args) {
+        /*
+        *Created an object from IAddition interface and applied its method in order to define the sum operation.
+        */
+        IAddition sumOperation = (x, y) -> x+y;
+        /*
+         *Created an object from ISubtraction interface and applied its method in order to define the subtraction operation.
+         */
+        ISubtraction subtractionOperation = (x, y) -> x-y;
 
+        System.out.println( sumOperation.calculateSum(10.0,5.0));
+        System.out.println( subtractionOperation.calculateSubtraction(50.0,5.0));
+
+//        System.out.println("Subtraction: " + operation.calculateSubtraction(8.0, 2.5));
+//        System.out.println("Multiplication: " + operation.calculateMultiplication(2.0, 5.5));
+//        System.out.println("Division: " + operation.calculateDivision(50.0, 3.0));
     }
 }
+
